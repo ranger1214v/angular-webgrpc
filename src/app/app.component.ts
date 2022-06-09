a7e9f63869ae3cedbc1f5138a9fdf9c0202868ab
ranger1214v <ranger1214v@gmail.com>
2022-06-09 18:55:19 +0800

    2 files modified, 7 added, 7 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,44 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Filter, Item } from 'proto/generated/proto/action_pb';
-import { ToDoServiceClient } from 'proto/generated/proto/action_pb_service';
+import { Filter, Item } from './proto/generated/proto/action_pb';
+import { ToDoServiceClient } from './proto/generated/proto/action_pb_service';
 import { Subject } from 'rxjs';
-import { scan, tap, } from 'rxjs/operators';
-
+import { scan, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   @ViewChild('listBox', { static: true }) listBox: ElementRef | undefined;
 
-  public clientWeb = new ToDoServiceClient('https://node-grpc-envoy-dnz3lqp74q-de.a.run.app');
+  public clientWeb = new ToDoServiceClient(
+    'https://node-grpc-envoy-dnz3lqp74q-de.a.run.app'
+  );
 
   public messageItem$ = new Subject<Item.AsObject>();
   public messageList$ = this.messageItem$.pipe(
-    scan((current: Item.AsObject[], newItem: Item.AsObject) => [...current, newItem], []),
+    scan(
+      (current: Item.AsObject[], newItem: Item.AsObject) => [
+        ...current,
+        newItem,
+      ],
+      []
+    )
   );
 
-  constructor() {
-  }
+  constructor() {}
 
   async ngOnInit() {
-
     const filter = new Filter();
     this.clientWeb.serverStreamingSubList(filter).on('data', (resultItem) => {
       const result = resultItem.toObject();
       this.messageItem$.next(result);
     });
 
-    this.clientWeb.serverStreamingSubList(filter).on('end', () => alert('與聊天室連線已中斷'));
-
+    this.clientWeb
+      .serverStreamingSubList(filter)
+      .on('end', () => alert('與聊天室連線已中斷'));
   }
 
   /**
@@ -43,11 +49,12 @@ export class AppComponent implements OnInit {
     const item = new Item();
     item.setName(message);
     item.setPrice(100);
-    this.clientWeb.unaryAddItem(item, (err, response) => console.log('clientWeb.unaryAddItem End', { response, err }));
+    this.clientWeb.unaryAddItem(item, (err, response) =>
+      console.log('clientWeb.unaryAddItem End', { response, err })
+    );
   }
 
   scrollToBottom = (elm: HTMLElement) => {
     elm.scrollTo({ top: elm.scrollHeight });
   };
-
 }
