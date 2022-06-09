ec6d22c73a262950e32cbabee388c28556a62c2e
ranger1214v <ranger1214v@gmail.com>
2022-06-09 20:56:11 +0800

    1 file modified, 7 added, 7 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,39 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Filter, Item } from 'proto/generated/proto/action_pb';
-import { ToDoServiceClient } from 'proto/generated/proto/action_pb_service';
+import { Filter, Item } from './proto/generated/proto/action_pb';
+import { ToDoServiceClient } from './proto/generated/proto/action_pb_service';
 import { BehaviorSubject, Subject } from 'rxjs';
-import { scan, tap, } from 'rxjs/operators';
-
+import { scan, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   @ViewChild('listBox', { static: true }) listBox: ElementRef | undefined;
 
-  public clientWeb = new ToDoServiceClient('https://node-grpc-envoy-dnz3lqp74q-de.a.run.app');
+  public clientWeb = new ToDoServiceClient(
+    'https://node-grpc-envoy-dnz3lqp74q-de.a.run.app'
+  );
 
   public name$ = new BehaviorSubject('沒有名字');
   public messageItem$ = new Subject<Item.AsObject>();
   public messageList$ = this.messageItem$.pipe(
-    scan((current: Item.AsObject[], newItem: Item.AsObject) => [...current, newItem], []),
+    scan(
+      (current: Item.AsObject[], newItem: Item.AsObject) => [
+        ...current,
+        newItem,
+      ],
+      []
+    )
   );
 
-  constructor() {
-  }
+  constructor() {}
 
   async ngOnInit() {
-    this.name$.next(localStorage.getItem('name') ?? prompt('請輸入你的名字') ?? '沒有名字');
+    this.name$.next(
+      localStorage.getItem('name') ?? prompt('請輸入你的名字') ?? '沒有名字'
+    );
     localStorage.setItem('name', this.name$.value);
 
     const filter = new Filter();
@@ -34,8 +42,9 @@ export class AppComponent implements OnInit {
       this.messageItem$.next(result);
     });
 
-    this.clientWeb.serverStreamingSubList(filter).on('end', () => alert('與聊天室連線已中斷'));
-
+    this.clientWeb
+      .serverStreamingSubList(filter)
+      .on('end', () => alert('與聊天室連線已中斷'));
   }
 
   /**
@@ -47,11 +56,12 @@ export class AppComponent implements OnInit {
     const item = new Item();
     item.setName(name);
     item.setMessage(message);
-    this.clientWeb.unaryAddItem(item, (err, response) => console.log('clientWeb.unaryAddItem End', { response, err }));
+    this.clientWeb.unaryAddItem(item, (err, response) =>
+      console.log('clientWeb.unaryAddItem End', { response, err })
+    );
   }
 
   scrollToBottom = (elm: HTMLElement) => {
     elm.scrollTo({ top: elm.scrollHeight });
   };
-
 }
