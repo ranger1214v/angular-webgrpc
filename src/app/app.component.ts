2a5a4a1c96767281b3872abf698054d6ca40931b
黃偉 <ranger1214v@gmail.com>
2022-09-05 15:21:08 +0800

    調整切換 RPC mode demo 的操作方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,25 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class AppComponent implements OnInit {
   @ViewChild('listBox', { static: true }) listBox: ElementRef | undefined;
 
-  constructor() {
+  public selectedRoute = 'unary';
+
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router,
+  ) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe((event: any) => {
+      this.selectedRoute = event.url.replace(/^\//, '');
+    });
+
   }
 
   async ngOnInit() {
+  }
+
+  changeRoute() {
+    this.router.navigate([this.selectedRoute], { queryParams: {} });
   }
 
 }
